b780e661a624fd9c8c1858567b7e64297c80fa33
Arman Sheikh Ali <arman.ali@qlik.com>
2016-01-13 09:15:49 +0100

    Code refactored.

--- src/properties.js
+++ src/properties.js
@@ -3,44 +3,16 @@ define( [
 	'jquery',
 	'underscore',
 	'qlik',
-	'ng!$http'
-], function ( $, _, qlik, $http ) {
+	'ng!$http',
+	'ng!$q'
+], function ( $, _, qlik, $http, $q ) {
 
 	return {
 
 		fetch: function () {
-			//console.log( "fetch called" );
-			//
-			//var url = '/path/to/resource';
-			//
-			//$http.get( url ).success( function ( response ) {
-			//	console.log( JSON.stringify( response ) );
-			//} );
-
 			$http.get( "/base/src/hello.html" );
-		},
-
-		fetch2: function () {
-			var injector = angular.injector( ['ng'] );
-
-			var $http = injector.get( '$http' );
-			var $rootScope = injector.get( '$rootScope' );
-
-			$rootScope.$apply( function () {
-				$http.get( "/base/src/hello.html" )
-					.then( function ( response ) {
-						var names = response.data;
-						console.log( "fetch 2:" );
-						console.log( JSON.stringify( names ) );
-					} );
-			} );
 		}
 
 	};
-
-	// ****************************************************************************************
-	// Return Values
-	// ****************************************************************************************
-	//return panelDefinition;
 
 } );
--- test/unit/sample.test.js
+++ test/unit/sample.test.js
@@ -1,73 +1,17 @@
 define( ['angular', 'angularmocks', 'properties', 'ng!$http', 'sinon'],
 	function ( angular, angularmocks, props, $http, sinon ) {
 		describe( 'Dummy test', function () {
-			//var httpBackend, http;
-
-			//beforeEach( function () {
-			//	//module( 'ng' );
-			//	inject( function ( $httpBackend, $http ) {
-			//		httpBackend = $httpBackend;
-			//		http = $http;
-			//
-			//		$http.get("www.google.se").then( function ( response ) {
-			//			console.log( "success" );
-			//		}, function ( response ) {
-			//			console.log( JSON.stringify( response ) );
-			//		} );
-			//	} );
-			//} );
-
-			//it( 'should fetch page', function () {
-			//	inject( function ( $httpBackend ) {
-			//		console.log( "Expecting get" );
-			//		$httpBackend.expectGET( '/abc' ).respond( "RESULT" );
-			//		props.fetch();
-			//		console.log( "Expecting flush" );
-			//		$httpBackend.flush();
-			//	} );
-			//} );
-
-			//it( "expects GET http calls and returns mock data", inject( function ( $httpBackend ) {
-			//	var url = '/path/to/resource';
-			//
-			//	// Create expectation
-			//	$httpBackend.expectGET( "/base/src/hello.html" ).respond( 200, 'mock data' );
-			//
-			//	//// Call http service
-			//	//$http.get( url ).success( function ( response ) {
-			//	//	console.log( JSON.stringify( response ) );
-			//	//} );
-			//
-			//	$http( {
-			//		method: 'GET',
-			//		url: url
-			//	} ).then( function successCallback ( response ) {
-			//		console.log( "success" );
-			//	}, function errorCallback ( response ) {
-			//		console.log( "error" );
-			//	} );
-			//
-			//	// flush response
-			//	$httpBackend.flush();
-			//
-			//} ) );
 
 			it( "test", function () {
-
 				sinon.stub( $http, "get", function ( url ) {
-
 					console.log( "stub called for $http" );
-
-					//$rootScope.$apply( function () {
-					//	$http.get( url )
-					//		.then( function ( response ) {
-					//			console.log( response.data );
-					//		} );
-					//} );
 				} );
 
 				props.fetch();
-				console.log( "test 1" );
+
+				sinon.assert.called( $http.get );
+
+				$http.get.restore();
 			} );
 
 		} );
